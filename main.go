c26e4db467aeb27070db9191db7da75d2b63732f
Azu0925 <yamaneko564783@gmail.com>
2021-11-13 11:53:12 +0900

    html表示

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	engine := gin.Default()
+	ua := ""
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
+	engine.LoadHTMLGlob("templates/*")
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
+			"message":    "hello world",
+			"User-Agent": ua,
+		})
+	})
+	engine.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"message": "hello gin",
 		})
 	})
 	engine.Run(":3000")
