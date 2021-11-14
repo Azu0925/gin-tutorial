2820f21a577f9285028c40bf01d841587ecf4e02
Azu0925 <yamaneko564783@gmail.com>
2021-11-14 21:50:01 +0900

    ファイルアップロードまで

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"net/http"
 
+	"io"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
+	engine.Static("/static", "./static")
 	ua := ""
 	engine.Use(func(c *gin.Context) {
 		ua = c.GetHeader("User-Agent")
@@ -23,6 +28,27 @@ func main() {
 	engine.GET("/home", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"message": "hello gin",
+		})
+	})
+	engine.POST("/upload", func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("image")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		fileName := header.Filename
+		dir, _ := os.Getwd()
+		out, err := os.Create(dir + "\\images\\" + fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
 		})
 	})
 	engine.Run(":3000")
